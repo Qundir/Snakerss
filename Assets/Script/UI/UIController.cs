57c1b6e23326222117a2efcad45c8bea179096fb
Blinkz-AL <erdem-300@hotmail.com>
2024-03-28 01:13:37 +0300

    Sound settings save
    add for PlayerPrefs for the audio settings

--- Assets/Script/UI/UIController.cs
+++ Assets/Script/UI/UIController.cs
@@ -6,20 +6,46 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
     public Slider _musicSlider, _sfxSlider;
+
+    private void Start()
+    {
+        // PlayerPrefs'ten kayýtlý müzik ve ses efekti seviyelerini kontrol edin
+        if (PlayerPrefs.HasKey("MusicVolume"))
+        {
+            float musicVolume = PlayerPrefs.GetFloat("MusicVolume");
+            _musicSlider.value = musicVolume;
+            AudioManager.Instance.MusicVolume(musicVolume);
+        }
+
+        if (PlayerPrefs.HasKey("SFXVolume"))
+        {
+            float sfxVolume = PlayerPrefs.GetFloat("SFXVolume");
+            _sfxSlider.value = sfxVolume;
+            AudioManager.Instance.SFXVolume(sfxVolume);
+        }
+    }
+
     public void ToggleMusic()
     {
         AudioManager.Instance.ToggleMusic();
     }
+
     public void ToggleSFX()
     {
         AudioManager.Instance.ToggleSFX();
     }
+
     public void MusicVolume()
     {
-        AudioManager.Instance.MusicVolume(_musicSlider.value);
+        float musicVolume = _musicSlider.value;
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume); // Müzik seviyesini PlayerPrefs'e kaydet
+        AudioManager.Instance.MusicVolume(musicVolume);
     }
+
     public void SFXVolume()
     {
-        AudioManager.Instance.SFXVolume(_sfxSlider.value);
+        float sfxVolume = _sfxSlider.value;
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume); // SFX seviyesini PlayerPrefs'e kaydet
+        AudioManager.Instance.SFXVolume(sfxVolume);
     }
 }
